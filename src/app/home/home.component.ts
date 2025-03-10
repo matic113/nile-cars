4bebf33dee412d370bb068c083b0a5909f88c8c4
farouk mohamed <faroukalghannam@gmail.com>
2025-03-10 19:12:32 +0200

    test angular matrial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatProgressSpinnerModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  isLoading = true; // Controls loading state
 
+  constructor() {
+    // Simulate data loading for 3 seconds
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
 }
